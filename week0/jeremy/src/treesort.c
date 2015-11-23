0081d2ce1c19d9fab4c00a615465d02ecbd20426
Jeremy Novak <jnovak17@csl08-l.cornellcollege.edu>
2015-11-23 15:30:15 -0600

    1st day's work (treesort NOT done)

--- week0/jeremy/src/treesort.c
+++ week0/jeremy/src/treesort.c
@@ -1,6 +1,6 @@
 
 // treesort.c
-// Norbert Wiener
+// Jeremy Novak
 // CSC311 Systems Software
 // 23 November 2015
 
@@ -8,30 +8,87 @@
 #include <stdlib.h>
 
 // define a node in a binary search tree
+struct node{
+  int value;
+  struct node* lp;
+  struct node* rp;
+};
 
 // define aliases
+typedef struct node Node;
+typedef struct node* NodePointer;
 
 // define a function that will create
 // a new tree that contains only a single
 // node
+NodePointer createRoot( int n ){
+  NodePointer np = (NodePointer) malloc( sizeof(Node));
+  np->value = n;
+  np->lp = NULL;
+  np->rp = NULL;
+  return np;
+}//createRoot(int)
 
 // define a function that will add a new
 // node to an existing binary search tree
+NodePointer addNode( NodePointer np, int n){
+  NodePointer rp = np; 
 
+  if(np ==NULL){
+    rp = createRoot(n);
+  }//if
+  else if(n< np->value){
+    if(np->lp == NULL){
+      np->lp = createRoot(n);
+    }//if
+    else {
+      np-> lp = addNode(np->lp, n);
+    }//else
+  }//else if
+  else if(n> np->value){
+    if(np->rp == NULL){
+      np->rp = createRoot(n);
+    }//if
+    else {
+      np->rp = addNode( np->rp, n);
+    }//else *************************Bottom of page 5
+  }//else if
+  else {
     // if value to be added is already in tree, do nothing
     // (do not add duplicate values to tree)
+  }// /else
+  return rp;
+}//addNode (NodePointer np)
 
 // traverse a binary search tree
 // in order, printing the values
 // found at each node
+void printNodes(NodePointer np){
+  if(np!=NULL){
+    printNodes(np->lp);
+    printf("value at node = %4d\n", np->value);
+    printNodes(np->rp);
+  }//if
+}//printNodes(NodePointer np)
 
 int main( int argc, char** argv ) {
 
   // build a binary search tree that
   // contains integers found on the command line,
   // then print the integers in ascending order
+  if(argc >1){
 
-  printf( "Hello from treesort!\n" );
+    NodePointer rp = createRoot( atoi(argv[1]) );
 
-  exit(0);
+    int i; 
+    for(i=2; i<argc;i++){
+      rp = addNode( rp, atoi(argv[i]) );
+
+    }//for
+	printNodes(rp);
+  }//if
+
+  // printf( "Hello from treesort!\n" );
+
+  exit(0); 
 } // main( int, char** )
