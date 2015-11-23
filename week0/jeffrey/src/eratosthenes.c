454e9383f1e2aec81b4654fe96f8763589da4af1
Jeff Klow <jklow17@cornellcollege.edu>
2015-11-23 14:48:16 -0600

    Wrote and compiled code for eratosthenes, schedules, and treesort. All three tested and working.

--- week0/jeffrey/src/eratosthenes.c
+++ week0/jeffrey/src/eratosthenes.c
@@ -1,42 +1,57 @@
-
 // eratosthenes.c
-// Norbert Wiener
+// Jeffrey Klow
 // CSC311 Systems Software
-// 23 November 2015
+// Nov. 23 2015
 
 #include <stdio.h>
 #include <stdlib.h>
 
-// this program will identify all
+// This program will identify all 
 // prime numbers that are < SIZE
+#define SIZE 1000000
 
-// filter() executes the Sieve of Eratosthenes
-// algorithm
-  
-  // initialize bitmap (array of zeroes and ones)
-  // 0 and 1 are not prime
-  // begin by assuming that any integer >= 2 could
-  // be prime
+int bitmap[SIZE];
 
-    // bitmap[i] = 0 means i could be prime
-    // (the next loop will determine if i is really prime)
+// filter() executes the Sieve of Eratosthenes algorithm
+void filter() {
+  int i = 0;
+  int j = 2;
 
+  // Initialize bitmap as all zeroes
+  // 0s are prime, 1s are not prime
+  // Note that 0 and 1 are not prime
+  // Assume that any value >1 can be prime
+  bitmap[0] = 1;
+  bitmap[1] = 1;
 
+  for( i = 2; i < SIZE; i++ ) {
+    bitmap[i] = 0;
+  } // end bitmap init
 
-  // 2 is the smallest prime number so
-  // start the search for prime numbers
-  // at 2
+  // Begin searching for primes
+  // 2 is smallest prime, start there
+  i = 2;
+  while( i < SIZE ) {
+    if( bitmap[i] == 0 ) {
+      for( j = i + i; j < SIZE; j += i) {
+        bitmap[j] = 1;
+      } // end for
+    } // end if
 
-      // if i is prime, then all of its 
-      // multiples are composite (not prime)
-
+    i++;
+  } // end while
+} // end filter()
 
 int main( int argc, char** argv ) {
-  // find all of the prime numbers < SIZE
+  // Find all primes < SIZE
+  filter();
 
-  // print all of the prime numbers < SIZE
-
-  printf( "Hello from eratosthenes!\n" );
-
+  // Print results
+  int i;
+  for( i = 0; i < SIZE; i++ ) {
+    if( bitmap[i] == 0) {
+      printf( "%d is prime.\n", i);
+    }
+  }
   exit(0);
-} // main( int, char** )
+} // end main
